ea81530c33701806bd77bbac9688cca8c69714dd
sschmidt23 <sschmidt@physics.ucdavis.edu>
2023-06-07 15:08:13 -0700

    add nondetect_replace config option to switch to original algo setup

--- src/rail/estimation/algos/cmnn.py
+++ src/rail/estimation/algos/cmnn.py
@@ -40,7 +40,9 @@ class Inform_CMNNPDF(CatInformer):
                           err_bands=SHARED_PARAMS,
                           redshift_col=SHARED_PARAMS,
                           mag_limits=SHARED_PARAMS,
-                          nondetect_val=SHARED_PARAMS)
+                          nondetect_val=SHARED_PARAMS,
+                          nondetect_replace=Param(bool, True, msg="set to True to replace non-detects,"
+                                                  " False to ignore in distance calculation"))
 
     def __init__(self, args, comm=None):
         """ Constructor
@@ -50,26 +52,31 @@ class Inform_CMNNPDF(CatInformer):
     def run(self):
         if self.config.hdf5_groupname:
             training_data = self.get_data('input')[self.config.hdf5_groupname]
-        else:  #pragma: no cover
+        else:  # pragma: no cover
             training_data = self.get_data('input')
         specz = np.array(training_data[self.config['redshift_col']])
 
         # replace nondetects
-        for col,err in zip(self.config.bands, self.config.err_bands):
+        for col, err in zip(self.config.bands, self.config.err_bands):
             if np.isnan(self.config.nondetect_val):  # pragma: no cover
                 mask = np.isnan(training_data[col])
             else:
                 mask = np.isclose(training_data[col], self.config.nondetect_val)
-            training_data[col][mask] = self.config.mag_limits[col]
-            training_data[err][mask] = 1.0  # could also put 0.757 for 1 sigma, but slightly inflated seems good
+            if self.config.nondetect_replace:
+                training_data[col][mask] = self.config.mag_limits[col]
+                training_data[err][mask] = 1.0  # could also put 0.757 for 1 sigma, but slightly inflated seems good
+            else:
+                training_data[col][mask] = np.nan
+                training_data[err][mask] = np.nan
 
         col_data, col_err = _computecolordata(training_data,
                                               self.config.bands,
                                               self.config.err_bands)
 
-        self.model = dict(train_color=col_data, train_err=col_err, truez=specz)
+        self.model = dict(train_color=col_data, train_err=col_err, truez=specz,
+                          nondet_choice=self.config.nondetect_replace)
         self.add_data('model', self.model)
-        
+
 
 class CMNNPDF(CatEstimator):
     """Color Matched Nearest Neighbor Estimator
@@ -108,7 +115,7 @@ class CMNNPDF(CatEstimator):
                           redshift_col=SHARED_PARAMS,
                           seed=Param(int, 66, msg="random seed used in selection mode"),
                           ppf_value=Param(float, 0.68, msg="PPF value used in Mahalanobis distance"),
-                          selection_mode=Param(int, 1, msg="select which mode to choose the redshift estimate:" +
+                          selection_mode=Param(int, 1, msg="select which mode to choose the redshift estimate:"
                                                "0: randomly choose, 1: nearest neigh, 2: weighted random"),
                           min_n=Param(int, 25, msg="minimum number of training galaxies to use"),
                           min_thresh=Param(float, 0.0001, msg="minimum threshold cutoff"),
@@ -135,39 +142,62 @@ class CMNNPDF(CatEstimator):
         self.train_color = self.model['train_color']
         self.train_err = self.model['train_err']
         self.truez = self.model['truez']
+        self.nondet_choice = self.model['nondet_choice']
 
     def _process_chunk(self, start, end, data, first):
         print(f"Process {self.rank} estimating PZ PDF for rows {start:,} - {end:,}")
         # replace nondetects
-        for col,err in zip(self.config.bands, self.config.err_bands):
+        for col, err in zip(self.config.bands, self.config.err_bands):
             if np.isnan(self.config.nondetect_val):  # pragma: no cover
                 mask = np.isnan(data[col])
             else:
                 mask = np.isclose(data[col], self.config.nondetect_val)
-            data[col][mask] = self.config.mag_limits[col]
-            data[err][mask] = 1.0  # could also put 0.757 for 1 sigma, but slightly inflated seems good
-        
+            if self.nondet_choice:
+                data[col][mask] = self.config.mag_limits[col]
+                data[err][mask] = 1.0  # could also put 0.757 for 1 sigma, but slightly inflated seems good
+            else:
+                data[col][mask] = np.nan
+                data[err][mask] = np.nan
+
         test_color, test_err = _computecolordata(data,
                                                  self.config.bands,
                                                  self.config.err_bands)
         num_gals = test_color.shape[0]
+        ncols = test_color.shape[1]
+        # chi2.ppf calculation is slow and repeated a bunch if nondet_choice is false
+        # so precompute the potential values here and put in a lookup table
+        thresh_table = np.zeros(ncols + 1)
+        thresh_table[0] = 0.0  # this will never be used, just put in a zero
+        for ii in range(1, ncols + 1):
+            thresh_table[ii] = chi2.ppf(self.config.ppf_value, float(ii))
         chunk_pz = np.zeros(num_gals)
         chunk_pze = np.zeros(num_gals)
         chunk_ncm = np.zeros(num_gals, dtype=int)
-        rng = np.random.default_rng(seed = self.config.seed + start)
+        chunk_colused = np.zeros(num_gals, dtype=int)
+        rng = np.random.default_rng(seed=self.config.seed + start)
         for ii in range(num_gals):
-
             MahalanobisDistance = np.nansum((test_color[ii] - self.train_color)**2 / test_err[ii]**2, axis=1, dtype='float')
             # original CMNN algorithm determines degrees of freedom in a roundabout way that takes into account
             # non-detect bands.  I think with the replacement of non-detects with 1 sigma limit and large error
             # we should be ok to set this to just be num_bands - 1
-            deg_of_freedom = len(self.config.bands) - 1
-            threshold = chi2.ppf(self.config.ppf_value, deg_of_freedom)
-
+            if self.nondet_choice:
+                deg_of_freedom = len(self.config.bands) - 1
+                threshold = chi2.ppf(self.config.ppf_value, deg_of_freedom)
+                chunk_colused[ii] = deg_of_freedom
+            else:
+                doftest = ~np.isnan(test_color[ii])
+                deg_of_freedom = np.nansum((test_color[ii]**2 + self.train_color**2 + 1.0) / (test_color[ii]**2 + self.train_color**2 + 1.0),
+                                           axis=1, dtype='int')
+                ntrain = len(deg_of_freedom)
+                # this is the slow way to do things, comment out and replace!
+                # threshold = np.array([chi2.ppf(self.config.ppf_value, deg_of_freedom[xx]) for xx in range(ntrain)])
+                # lookup table-based way to get thresholds
+                threshold = np.array([thresh_table[deg_of_freedom[xx]] for xx in range(ntrain)])
+                chunk_colused[ii] = np.sum(doftest)
             # find the indices of galaxies meeting CMNN subset criteria
-            index = np.where((threshold > self.config.min_thresh) & \
-                             (MahalanobisDistance > self.config.min_dist) & \
-                             (MahalanobisDistance <= threshold) )[0]
+            index = np.where((threshold > self.config.min_thresh) &
+                             (MahalanobisDistance > self.config.min_dist) &
+                             (MahalanobisDistance <= threshold))[0]
 
             if len(index) >= self.config.min_n:
 
@@ -188,7 +218,7 @@ class CMNNPDF(CatEstimator):
                         rval = rng.choice(tx, size=1, replace=False)[0]
                     out_pz = self.truez[index[rval]]
                     out_pze = np.std(self.truez[index])
-                    del tx,rval
+                    del tx, rval
 
                 # weight by how good the color match is and then choose randomly
                 if self.config.selection_mode == 2:
@@ -210,13 +240,19 @@ class CMNNPDF(CatEstimator):
                     sx = np.argsort(tempMD)
                     new_MD = np.asarray(tempMD[sx[0:self.config.min_n]], dtype='float')
                     new_TZ = np.asarray(tempTZ[sx[0:self.config.min_n]], dtype='float')
-                    del tempMD, tempTZ, sx
-                    ### calculate the new 'effective PPF' based on the most distant nearest neighbor
-                    new_ppf_value = chi2.cdf(new_MD[-1], deg_of_freedom)
-                    ### inflate the photo-z error appropriately
-                    temp = np.std( new_TZ )
+                    if self.nondet_choice:
+                        lim_DOF = deg_of_freedom
+                    else:
+                        tmpDOF = deg_of_freedom[index2]
+                        new_DOF = np.asarray(tmpDOF[sx[0:self.config.min_n]], dtype='float')
+                        lim_DOF = new_DOF[-1]
+                    del tempMD, tempTZ, sx, new_DOF
+                    # calculate the new 'effective PPF' based on the most distant nearest neighbor
+                    new_ppf_value = chi2.cdf(new_MD[-1], lim_DOF)
+                    # inflate the photo-z error appropriately
+                    temp = np.std(new_TZ)
                     out_pze = temp * (new_ppf_value / self.config.ppf_value)
-                    del temp,new_ppf_value
+                    del temp, new_ppf_value
                     if self.config.selection_mode == 0:
                         rval = rng.choice(self.config.min_n, size=1, replace=False)[0]
                         out_pz = new_TZ[rval]
@@ -229,7 +265,7 @@ class CMNNPDF(CatEstimator):
                         cx = rng.choice(self.config.min_n, size=1, replace=False, p=weights)[0]
                         out_pz = new_TZ[cx]
                         del tweights, weights, cx
-                    del new_MD,new_TZ
+                    del new_MD, new_TZ
                     Ncm = self.config.min_n
                 else:  # pragma: no cover
                     # I think this would only happen if there are less than min_n gals in training set
@@ -241,5 +277,5 @@ class CMNNPDF(CatEstimator):
             chunk_ncm[ii] = Ncm
         ens = qp.Ensemble(qp.stats.norm, data=dict(loc=np.expand_dims(chunk_pz, -1),
                                                    scale=np.expand_dims(chunk_pze, -1)))
-        ens.set_ancil(dict(zmode=chunk_pz, Ncm=chunk_ncm))
+        ens.set_ancil(dict(ncolors=chunk_colused, zmode=chunk_pz, Ncm=chunk_ncm))
         self._do_chunk_output(ens, start, end, first)
